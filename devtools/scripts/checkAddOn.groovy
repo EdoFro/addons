91b7050aaaeabd400513db69a26a90367bb77986
EdoFro <35700575+EdoFro@users.noreply.github.com>
2022-01-19 18:56:35 -0300

    Added "actions" node
    it adds a node with links to the Build and to the Package devtools menu commands

--- devtools/scripts/checkAddOn.groovy
+++ devtools/scripts/checkAddOn.groovy
@@ -607,6 +607,22 @@ filesToUninstall.addAll(
 )
 
 //
+// ============ actions ============
+//
+def actionsNode = findOrCreate(root, 'actions', RIGHT)
+actionsNode.note = withBody '''
+ Direct links to menu commands
+'''
+def actions = ['addons.devtools.checkAddOn','addons.devtools.releaseAddOn']
+actions.each{ acc ->
+    def labelText = textUtils.getText(acc)
+    if (!actionsNode.children.any{it.text == labelText}) {        
+        def n = actionsNode.createChild(labelText)
+        n.link.text = "menuitem:_${acc}_on_single_node"
+    }
+}
+
+//
 // ============ uninstallation rules ============
 //
 filesToUninstall = filesToUninstall.collect { '${installationbase}/' + it.replace('\\', '/') }
